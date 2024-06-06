cfc69b8d987b4fb60f0bb98ce6c15e7b338361ca
yaoska telleria <yaoskatelleria59@gmail.com>
2024-06-05 19:00:22 -0600

    Estructura inicial de nuestro proyecto

---
+++ src/Pages/App/index.jsx
@@ -0,0 +1,19 @@
+// import '../../../src/App.css'
+import { BrowserRouter, useRoutes } from 'react-router-dom'
+import { NotFound } from '../../NotFound'
+import { Home } from '../Home'
+import { MyAccount } from '../MyAccount'
+import { MyOrder } from '../MyOrder'
+import { MyOrders } from '../MyOrders'
+import { SingIn } from '../SingIn'
+import './App.css'
+
+function App() {
+  return (
+    <>
+      
+    </>
+  )
+}
+
+export default App
---
+++ src/Pages/Home/index.jsx
@@ -0,0 +1,11 @@
+function Home() {
+    return (
+        <>
+            <p className='bg-red-100'>
+                home
+            </p>
+        </>
+    )
+}
+
+export { Home }
